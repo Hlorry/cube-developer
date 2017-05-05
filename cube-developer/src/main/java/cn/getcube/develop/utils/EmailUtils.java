c6f9c7183fdf2185ec40b647b9631701d0ba368b
SubDong <shuzidong@shixinyun.com>
2017-05-05 15:51:06 +0800

    [fix] 临时提交

--- cube-developer/src/main/java/cn/getcube/develop/utils/EmailUtils.java
+++ cube-developer/src/main/java/cn/getcube/develop/utils/EmailUtils.java
@@ -71,7 +71,7 @@ public class EmailUtils {
         //参数length，表示生成几位随机数
         for(int i = 0; i < length; i++) {
 
-            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";
+            String charOrNum = random.nextInt(2) % 2 == 0 ? "num" : "num";
             //输出字母还是数字
             if( "char".equalsIgnoreCase(charOrNum) ) {
                 //输出是大写字母还是小写字母
