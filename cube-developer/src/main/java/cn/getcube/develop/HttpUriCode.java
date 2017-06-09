5b2b5ee68648aea7eaee08117e18718c6ca44ac1
hulin <hulin@shixinyun.com>
2017-06-09 11:20:18 +0800

    修改ip端口号为域名

--- cube-developer/src/main/java/cn/getcube/develop/HttpUriCode.java
+++ cube-developer/src/main/java/cn/getcube/develop/HttpUriCode.java
@@ -7,6 +7,6 @@ public interface HttpUriCode {
     //项目公用uri
     //String HTTP_CODE_URI = "http://125.208.1.67:8180";
     //String HTTP_CODE_URI = "http://localhost:8080";
-    String HTTP_CODE_URI = "http://114.112.101.158:18100";
+    String HTTP_CODE_URI = "http://restful.getcube.cn";
 //    String LOCALHOST_TEST = "127.0.0.1:18100";
 }
